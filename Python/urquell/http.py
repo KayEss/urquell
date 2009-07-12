1c20b33ecf13bddb0d36477e47d5050f2224be34
Kirit Sælensminde <k@kirit.com>
2009-07-12 11:47:48 +0000

    Moved most of the function invocation code out of http.py.

--- Python/urquell/http.py
+++ Python/urquell/http.py
@@ -6,39 +6,9 @@ from google.appengine.api.urlfetch import fetch
 from google.appengine.api import memcache
 
 from jsonrpc.json import dumps, loads
+
 from urquell import Responder
-from urquell.invocation import Invocation
-import re, urllib, os
-
-REAL = re.compile('^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$')
-FUNCTION = re.compile('^=')
-def resolver_path(value):
-    value = urllib.unquote(value)
-    if FUNCTION.match(value):
-        return resolve_function(value)
-    elif value.startswith('=='):
-        value = value[1:]
-
-    if value.isdigit():
-        return (value, int(value))
-    elif REAL.match(value):
-        return (value, float(value))
-    return (value, value)
-
-def resolver_query(value):
-    return value
-
-
-def resolve_function(value):
-    value = value[1:]
-    dest = Invocation.gql("WHERE ihash = '%s'" % value).fetch(1)
-    if not len(dest):
-	    raise Exception("Unable to resolve function invocation. Invocation hash: %s" % value)
-    url = dest[0].url
-    json = memcache.get(value)
-    if not json:
-        json = fetch(url).content
-    return (url, loads(json)['value'])
+from urquell.invocation import store_invocation, resolver_path, resolver_query
 
 
 class Module(object):
@@ -87,22 +57,8 @@ class Module(object):
         module = self
         name = "%s/%s/.*" % (module.path(), fn.func_name)
         class Process(webapp.RequestHandler):
-            def store_invocation(self,a_url):
-                def gen_ihash():
-                    ihash = os.urandom(16).encode("base64")[:6]
-                    invocation = Invocation.gql("WHERE ihash = '%s'" % ihash).fetch(1)
-                    if invocation == False: return gen_ihash()
-                    return ihash
-                invocation = Invocation.gql("WHERE url = '%s'" % a_url).fetch(1)
-                if not len(invocation):
-                    ihash = gen_ihash()
-                    invocation = Invocation(ihash=ihash,url=a_url)
-                    db.put(invocation)
-                    return ihash
-                else:
-                    return invocation[0].ihash
             def doapply(self, path):
-                ihash = self.store_invocation(self.request.url)
+                ihash = store_invocation(self.request.url)
                 self.response.headers['Content-Type'] = 'text/plain'
                 parts = path[len(module.path()) + len(fn.func_name) + 2:].split('/')
                 call_trace = [resolver_path(str(i)) for i in parts]
--- Python/urquell/invocation.py
+++ Python/urquell/invocation.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 from google.appengine.ext import db
+from google.appengine.api import memcache
+
+from jsonrpc.json import dumps, loads
 
 """
 Example record
@@ -12,3 +16,50 @@ url: 'http://urquell-fn.appspot.com/lib/sub/3/4.json
 class Invocation(db.Model):
   ihash = db.StringProperty(required=True)
   url = db.StringProperty(required=True)
+
+def store_invocation(a_url):
+    def gen_ihash():
+        ihash = os.urandom(16).encode("base64")[:6]
+        invocation = Invocation.gql("WHERE ihash = '%s'" % ihash).fetch(1)
+        if len(invocation):
+            return gen_ihash()
+        return ihash
+    invocation = Invocation.gql("WHERE url = '%s'" % a_url).fetch(1)
+    if not len(invocation):
+        ihash = gen_ihash()
+        invocation = Invocation(ihash=ihash,url=a_url)
+        db.put(invocation)
+        return ihash
+    else:
+        return invocation[0].ihash
+
+import re, urllib, os
+
+REAL = re.compile('^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$')
+FUNCTION = re.compile('^=')
+def resolver_path(value):
+    value = urllib.unquote(value)
+    if FUNCTION.match(value):
+        return resolve_function(value)
+    elif value.startswith('=='):
+        value = value[1:]
+
+    if value.isdigit():
+        return (value, int(value))
+    elif REAL.match(value):
+        return (value, float(value))
+    return (value, value)
+
+def resolver_query(value):
+    return value
+
+def resolve_function(value):
+    value = value[1:]
+    dest = Invocation.gql("WHERE ihash = '%s'" % value).fetch(1)
+    if not len(dest):
+        raise Exception("Unable to resolve function invocation. Invocation hash: %s" % value)
+    url = dest[0].url
+    json = memcache.get(value)
+    if not json:
+        json = fetch(url).content
+    return (url, loads(json)['value'])
