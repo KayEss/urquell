9e14638dc648be1af4c80e82c5a328068ff935c0
Kirit Sælensminde <k@kirit.com>
2009-07-13 09:22:29 +0000

    Tidied up the layout of lib a bit. We now have a single file that contains both the implementations and bindings together.

--- Python/urquell/lib/__init__.py
+++ Python/urquell/lib/__init__.py
@@ -4,24 +4,80 @@ import test
 
 lib = Module(None, 'lib')
 
+def echo(*args, **kwargs):
+    """echo can be used to test how *args and **kwargs will be presented
+    to Python functions."""
+    return {
+        'arguments': args,
+        'keywords': kwargs,
+    }
 lib.pure(test.echo, [
-    '23/231.123123.json?hello=country&goodbye=nightclub',
+    '23/231.123123?hello=country&goodbye=nightclub',
     'hello%20country,%20goodbye%20nightclub',
 ])
 
+def add(a, *b):
+    """Add numbers"""
+    for v in b:
+        a = a + v
+    return a
 lib.pure(test.add, [
-    '1/2.json'
+    '1/2',
 ])
+def sub(a, *b):
+    """Subtract one number from another"""
+    for v in b:
+        a = a - v
+    return a
+    return a - b
 lib.pure(test.sub, [
-    '2/1.json'
+    '2/1',
 ])
+def mul(a, *b):
+    """Multiply a set of numbers together"""
+    for v in b:
+        a = a * v
+    return a
 lib.pure(test.mul, [
-    '2/1.json'
+    '2/1',
+])
+def div(a, *b):
+    """Divide a set of numbers"""
+    for v in b:
+        a = a / v
+    return a
+lib.pure(test.div, [
+    '2/3.0',
+    '2/3.0/6',
 ])
 
+def ifn(c, t, *f):
+    if bool(c):
+        return t
+    else:
+        return f
 lib.pure(test.ifn, [])
 
-lib.pure(test.fn, [])
-lib.pure(test.bind, [])
+def path_args(path):
+    return '/'.join([quote(unicode(p)) for p in path])
+
+def fn(server, *path):
+    return "http://%s/%s" % (server, path_args(path))
+lib.pure(test.fn, [
+    'urquell-fn.appspot.com/lib/echo/',
+    'urquell-fn.appspot.com/lib/add/1',
+])
+def bind(f, v):
+    return "%s/%s" % (f, v)
+lib.pure(test.bind, [
+])
+def call_trace(f, *path):
+    from urquell import execute
+    if len(path):
+        return execute('%s/%s' % (f, path_args(path)))
+    else:
+        return execute(f)
 lib.pure(test.call_trace, [])
+def call(f, *path):
+    return call_trace(f, *path)['value']
 lib.pure(test.call, [])
