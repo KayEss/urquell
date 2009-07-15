f269af02e730200fc1e9531b9274020eee855f42
Kirit Sælensminde <k@kirit.com>
2009-07-15 13:14:58 +0000

    Get in the HTTP module now works and the JSON content is now properly escaped on displayed.

--- Python/urquell/lib/http.py
+++ Python/urquell/lib/http.py
@@ -8,14 +8,27 @@ from google.appengine.api.urlfetch import fetch
 http = Module(lib, 'http', """HTTP functions.""")
 
 
-def get(url, *path, **kwargs):
-    """Does a HTTP get on the specified URL and returns the response."""
+def get(url):
+    """
+        <p>Does a HTTP get on the specified URL and returns the response. This will only return the body text if the resource is text.</p>
+        <p>If the encoding isn't properly specified in the target page's Content-Type then it's quite likely to error.</p>
+    """
     if url:
-        response = fetch(url)
-        return dict(
-            body = response.content,
-            headers = response.headers,
-        )
+        response = fetch(url, deadline=10)
+        headers = dict([(k, v) for k, v in response.headers.items()]);
+        mime, encoding = headers.get('content-type', '').split(';')
+        if len(encoding):
+            charset = encoding.split('=')[1]
+        if mime.startswith('text/'):
+            return dict(
+                body = response.content.decode(charset),
+                headers = headers,
+            )
+        else:
+            return dict(
+                body = None,
+                headers = headers,
+            )
     else:
         return None
 http.pure(get, [
