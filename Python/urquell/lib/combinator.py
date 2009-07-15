d1c5c5aa305ec8769d297615de1be80c5442d012
Kirit Sælensminde <k@kirit.com>
2009-07-15 10:02:19 +0000

    I think these I and K combinators are now sound.

--- Python/urquell/lib/combinator.py
+++ Python/urquell/lib/combinator.py
@@ -1,16 +1,42 @@
 # -*- coding: utf-8 -*-
 from urquell.http import Module
-from urquell.lib import lib
+from urquell.lib import lib, call
 from urquell.invocation import path_args
 
 
-combinator = Module(lib, 'combinator', """Various combinators.""")
+combinator = Module(lib, 'combinator', """Various combinators. All of the combinators throw away key word arguments.""")
 
 
-def I(v):
-    """The identity combinator. This function can only take a single argument."""
-    return v
+def I(v, *p, **k):
+    """
+        <p>The identity combinator.</p>
+        <p>This version of the identity combinator cannot work with both path arguments and query string arguments. If there is any path element at all then the query string is discarded. If the path is empty then the query string is returned as a JSON object.</p>
+    """
+    if not v and not len(p):
+        return k
+    elif len(p):
+        return [v] + list(p)
+    else:
+        return v
 combinator.pure(I, [
     '123',
     'hello%20world',
+    'hello/world',
+    '?hello=world',
+    'hello/world?hello=world',
 ])
+
+def K(f, *z, **kw):
+    """
+        <p>The K combinator. This is also known as the Kestrel.</p>
+        <p>K takes a function (resolved through a bind expression which will have been created via something like <a href="/lib/fn/">lib/fn</a>), executes the function over its other inputs and then returns the original function.</p>
+    """
+    call(f, *z, **kw)
+    return f
+combinator.pure(K, [
+])
+
+def S(*v):
+    """
+        <p>The S combinator.</p>
+    """
