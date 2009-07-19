7c129990aa34a4931176c631853bbe6191bbc5e0
Kirit Sælensminde <k@kirit.com>
2009-07-19 18:42:04 +0000

    Started to implement a simple URL parser and handler for generating Urquell style structured URLs from URLs and other parts for use with things like lib/call and lib/bind.

--- Python/urquell/url.py
+++ Python/urquell/url.py
@@ -5,6 +5,7 @@ class urlambda(object):
     def __init__(self, prefix, *path, **kwargs):
         parts = urlparse.urlsplit(prefix)
         self.prefix = '%s://%s/' % (parts[0], parts[1])
-        self.path = path
+        self.path = parts[2][1:].split('/') + list(path)
+        if len(self.path) == 1 and not self.path[0]:
+            self.path = self.path[1:]
         self.kwargs = kwargs
-
--- Tests/urlambda.py
+++ Tests/urlambda.py
@@ -4,11 +4,18 @@ sys.path.append('../Python')
 
 from urquell.url import urlambda
 
-class Constructors(unittest.TestCase):
-    def test_basic(self):
-        self.assert_(urlambda('http://www.example.com/').prefix == 'http://www.example.com/')
-        self.assert_(urlambda('http://www.example.com/path').prefix == 'http://www.example.com/')
-        self.assert_(urlambda('http://www.example.com:456/').prefix == 'http://www.example.com:456/')
+class URLParsing(unittest.TestCase):
+    def test_prefix(self):
+        self.assertEqual(urlambda('http://www.example.com/').prefix, 'http://www.example.com/')
+        self.assertEqual(urlambda('http://www.example.com/path').prefix, 'http://www.example.com/')
+        self.assertEqual(urlambda('http://www.example.com:456/').prefix, 'http://www.example.com:456/')
+
+    def test_path(self):
+        self.assertEqual(urlambda('http://www.example.com/').path, [])
+        self.assertEqual(urlambda('http://www.example.com/path').path, ['path'])
+        self.assertEqual(urlambda('http://www.example.com:456//').path, ['', ''])
+        self.assertEqual(urlambda('http://www.example.com:456//one').path, ['', 'one'])
+        self.assertEqual(urlambda('http://www.example.com:456//one/').path, ['', 'one', ''])
 
 if __name__ == '__main__':
     unittest.main()
