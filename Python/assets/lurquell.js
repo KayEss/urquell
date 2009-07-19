9e769d8d2609584f53c281332649e51c1e19ff24
Kirit Sælensminde <k@kirit.com>
2009-07-19 17:14:49 +0000

    This version of the gadget should now be able to fetch information from the Urquell server.

--- Python/assets/lurquell.js
+++ Python/assets/lurquell.js
@@ -2,8 +2,16 @@
 try {
     lurquell = function() {
         $('#lurquell').find('div').text("Gadget loaded. Requesting information from the Urquell server. Please wait... (you'll be waiting a long however as nothing is really happening)");
+
+        function start_line() {
+            $.getJSON("http://urquell-fn.appspot.com/?__=?", function(json) {
+                    $('#lurquell').find('div').text("Got something, not sure what though");
+                })
+        }
+
+        start_line();
     };
     lurquell();
 } catch ( e ) {
-    alert(e.description);
+    alert(e);
 }
