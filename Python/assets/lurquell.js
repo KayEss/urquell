24afa83ffe1ff1cc985a42594201282446b614a5
Kirit Sælensminde <k@kirit.com>
2009-07-25 10:08:42 +0000

    A simple gadget to show how the gadget UI mght look.

--- Python/assets/lurquell.js
+++ Python/assets/lurquell.js
@@ -1,17 +1,28 @@
 
-try {
-    lurquell = function() {
-        $('#lurquell').find('div').text("Gadget loaded. Requesting information from the Urquell server. Please wait...");
+lurquell = function () {
+    $('#lurquell').text("Gadget loaded. Requesting information from the Urquell server. Please wait...");
 
-        function start_line() {
-            $.getJSON("http://urquell-fn.appspot.com/?__=?", function(json) {
-                $('#lurquell').find('div').text("Got something, not sure what though");
-            })
-        }
+    function start_line() {
+        $.getJSON("http://urquell-fn.appspot.com/?__=?", function(json) {
+            $('#lurquell').replaceWith(
+                $('<div id="urquell">').append(
+                    $('<span id="urquell_server"></span>').text("http://urquell-fn.appspot.com/")
+                ).append(
+                    $("<select>").append(
+                        $('<option value="">').text("")
+                    ).append(
+                        $('<option value="null">').text("<null>")
+                    )
+                ).append(
+                    $('<input type="text" name="path">')
+                ).append(
+                    $('<input type="submit" value="Execute">')
+                )
+            );
+        })
+    }
 
-        start_line();
-    };
-    lurquell();
-} catch ( e ) {
-    alert(e);
+    start_line();
 }
+
+lurquell();
