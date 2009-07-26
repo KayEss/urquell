8082a48fabfc90e73c3e985e58f823c9f7a7e0f6
Kirit Sælensminde <k@kirit.com>
2009-07-26 06:50:06 +0000

    Restructured the code so that it can be extended more easily with a well defined external API.

--- Python/assets/lurquell.js
+++ Python/assets/lurquell.js
@@ -1,25 +1,31 @@
 
-lurquell = function () {
-    $('#lurquell').text("Gadget loaded. Requesting information from the Urquell server. Please wait...");
+$('#lurquell').text("Gadget loaded. Requesting information from the Urquell server. Please wait...");
 
-    function option_builder(name) {
-        return $('<option value="' + name + '">').text(name)
+lurquell = new function () {
+    /*
+        Builds the select box for a single level. This only works for modules.
+    */
+    function level_builder(json, level) {
+        function option_builder(name) {
+            return $('<option value="' + name + '">').text(name)
+        }
+        var opts = $('<select id="urquell_select_' + level + '">').append(
+            $('<option value="">').text( level ? "<null>" : "")
+        );
+        for ( var o in json.value.modules )
+            opts = opts.append(option_builder(json.value.modules[o].name))
+        return opts;
     }
-
-    function start_line() {
+    /*
+        Called to reset the gadget for the beginning of a line
+    */
+    this.start_line = function () {
         $.getJSON("http://urquell-fn.appspot.com/?__=?", function(json) {
             $('#lurquell').replaceWith(
                 $('<div id="urquell">').append(
                     $('<span id="urquell_server"></span>').text("http://urquell-fn.appspot.com/")
                 ).append(
-                    function(level) {
-                        var opts = $('<select id="urquell_select_' + level + '">').append(
-                            $('<option value="">').text( level ? "<null>" : "")
-                        );
-                        for ( var o in json.value.modules )
-                            opts = opts.append(option_builder(json.value.modules[o].name))
-                        return opts;
-                    }(0)
+                    level_builder(json, 0)
                 ).append(
                     $('<input type="text" name="path">')
                 ).append(
@@ -28,8 +34,7 @@ lurquell = function () {
             );
         })
     }
-
-    start_line();
+    return this;
 }
 
-lurquell();
+lurquell.start_line();
