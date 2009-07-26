e51e3ee6da1d7f8900f27e33e8db0aeb9f867594
Kirit Sælensminde <k@kirit.com>
2009-07-26 06:41:30 +0000

    The first level selection is now built from the server response.

--- Python/assets/lurquell.js
+++ Python/assets/lurquell.js
@@ -2,17 +2,24 @@
 lurquell = function () {
     $('#lurquell').text("Gadget loaded. Requesting information from the Urquell server. Please wait...");
 
+    function option_builder(name) {
+        return $('<option value="' + name + '">').text(name)
+    }
+
     function start_line() {
         $.getJSON("http://urquell-fn.appspot.com/?__=?", function(json) {
             $('#lurquell').replaceWith(
                 $('<div id="urquell">').append(
                     $('<span id="urquell_server"></span>').text("http://urquell-fn.appspot.com/")
                 ).append(
-                    $("<select>").append(
-                        $('<option value="">').text("")
-                    ).append(
-                        $('<option value="null">').text("<null>")
-                    )
+                    function(level) {
+                        var opts = $('<select id="urquell_select_' + level + '">').append(
+                            $('<option value="">').text( level ? "<null>" : "")
+                        );
+                        for ( var o in json.value.modules )
+                            opts = opts.append(option_builder(json.value.modules[o].name))
+                        return opts;
+                    }(0)
                 ).append(
                     $('<input type="text" name="path">')
                 ).append(
